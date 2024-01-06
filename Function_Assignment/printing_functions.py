03e69ae35d6a33e431184b7c5c46e8fdc31ab427
umarbshehu <umarbshehu@gmail.com>
2024-01-06 15:59:03 +0100

    Function Assignment

---
+++ Function_Assignment/printing_functions.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# printing_functions.py
+
+def print_models(unprinted_designs, completed_models):
+    """Simulate printing each design, until none are left.
+    Move each design to completed_models after printing."""
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nCompleted models:")
+    for model in completed_models:
+        print(model)
+
